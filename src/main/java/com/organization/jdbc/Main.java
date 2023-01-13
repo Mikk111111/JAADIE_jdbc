14542bc1234049bf31a316f432f21394437edd3e
mikk111111 <mik.k00259@gmail.com>
2023-01-13 10:14:08 +0200

    create list full

--- src/main/java/com/organization/jdbc/Main.java
+++ src/main/java/com/organization/jdbc/Main.java
@@ -1,5 +1,8 @@
 package com.organization.jdbc;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -9,8 +12,24 @@ public class Main {
                     "root1");
             Statement statement = conn.createStatement();
             ResultSet result = statement.executeQuery("select * from customers");
+            List<Customer> customers = new ArrayList<>();
+            while (result.next()) {
+                //System.out.println(result.getInt(1)+"|"+result.getString(2);
+                Customer customer = new Customer(result.getInt("customerNumber"),
+                        result.getString("customerName"),
+                        result.getString("phone"),
+                        result.getString("city"));
+                customers.add(customer);
+            }
+            customers.forEach(System.out::println);
+//            for (Customer elementas : customers){
+//                System.out.println(elementas);
+//            }
+//
+//            for (int i=0; i<customers.size(); i++){
+//                System.out.println(customers.get(i));
+//            }
 
-            while (result.next()) System.out.println(result.getInt(1)+"|"+result.getString(2));
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
