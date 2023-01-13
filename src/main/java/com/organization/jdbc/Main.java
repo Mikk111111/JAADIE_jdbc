e9a2bb8190c88415cf47b586e287992e9efd6c87
mikk111111 <mik.k00259@gmail.com>
2023-01-13 09:11:52 +0200

    connection fix

--- src/main/java/com/organization/jdbc/Main.java
+++ src/main/java/com/organization/jdbc/Main.java
@@ -3,14 +3,15 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            Connection c = DriverManager.getConnection(
+            Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/classicmodels",
                     "root",
                     "root1");
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from customers");
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery("select * from customers");
 
-            while (rs.next()) System.out.println(rs.getInt(1)+"|"+rs.getString(2));
+            while (result.next()) System.out.println(result.getInt(1)+"|"+result.getString(2));
+            conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
